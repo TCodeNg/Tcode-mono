8ef58796c2a5096a0f9b201db9e95029e31ad67c
Thompson Edolo <verygreenboi@live.com>
2020-05-30 12:49:50 +0100

    chore(storefront): create staging CD pipeline

---
+++ tools/docker/storefront/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+echo $PWD
+rm -rf ./dist/apps/tmp
+mkdir ./dist/apps/tmp
+cp ./dist/apps/storefront/* ./dist/apps/tmp
+rm -rf ./dist/apps/storefront/*
+cp tools/docker/storefront/nginx.conf dist/apps/storefront/nginx.conf
+cp tools/docker/storefront/Dockerfile dist/apps/storefront/Dockerfile
+
+mkdir ./dist/apps/storefront/build
+cp ./dist/apps/tmp/* ./dist/apps/storefront/build/
+rm -rf ./dist/apps/tmp
