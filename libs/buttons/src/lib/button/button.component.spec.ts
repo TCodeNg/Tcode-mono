d4b6e86ae4107fc23198945fb7ada6d63070e4b2
Sulaiman <sulaimain@kobo360.com>
2020-05-31 10:31:39 +0100

    chore(button): added test cases

--- libs/buttons/src/lib/button/button.component.spec.ts
+++ libs/buttons/src/lib/button/button.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import { MatIconModule } from '@angular/material/icon';
+import { By } from '@angular/platform-browser';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
+      declarations: [ ButtonComponent ],
+      imports: [
+        MatIconModule
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +26,64 @@ describe('ButtonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Normal Button', () => {
+
+    beforeEach(() => {
+      component.type = 'normal';
+      fixture.detectChanges();
+    });
+
+    it('should have a button', () => {
+      expect(
+        fixture.debugElement.query(By.css('.content'))
+          .query(By.css('button[mat-raised-button]'))
+      ).toBeTruthy();
+    });
+
+    it('should be disabled if disabled is truthy', () => {
+      component.disabled = true;
+      fixture.detectChanges();
+      const btn = fixture.debugElement.query(By.css('button[mat-raised-button]'));
+      expect(btn.properties.disabled).toBeTruthy();
+    });
+
+    it('should not be disabled if disabled is falsy', () => {
+      component.disabled = false;
+      fixture.detectChanges();
+      const btn = fixture.debugElement.query(By.css('button[mat-raised-button]'));
+      expect(btn.properties.disabled).toBeFalsy();
+    });
+
+  });
+
+  describe('Icon button', () => {
+    beforeEach(() => {
+      component.type = 'icon';
+      fixture.detectChanges();
+    });
+
+    it('should have a button', () => {
+      expect(
+        fixture.debugElement.query(By.css('.content'))
+          .query(By.css('button[mat-icon-button]'))
+      ).toBeTruthy();
+    });
+
+    it('should be disabled if disabled is truthy', () => {
+      component.disabled = true;
+      fixture.detectChanges();
+      const btn = fixture.debugElement.query(By.css('button[mat-icon-button]'));
+      expect(btn.properties.disabled).toBeTruthy();
+    });
+
+    it('should not be disabled if disabled is falsy', () => {
+      component.disabled = false;
+      fixture.detectChanges();
+      const btn = fixture.debugElement.query(By.css('button[mat-icon-button]'));
+      expect(btn.properties.disabled).toBeFalsy();
+    });
+  })
+
+  
 });
