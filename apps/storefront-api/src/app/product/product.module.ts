85aeb8a40d0a8b963f886376f36c2a2717ee365d
Thompson Edolo <verygreenboi@live.com>
2020-05-31 12:30:11 +0100

    chore(api): add product service

--- apps/storefront-api/src/app/product/product.module.ts
+++ apps/storefront-api/src/app/product/product.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductSchema } from '@tcode/api-interface';
+import { ProductService } from './product.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Product', schema: ProductSchema }
     ])
-  ]
+  ],
+  providers: [ProductService]
 })
 export class ProductModule {
 }
---
+++ libs/api-interface/src/lib/product.doc.ts
@@ -0,0 +1,19 @@
+import { Document } from 'mongoose';
+
+export class Product extends Document {
+  title: string;
+  price: number;
+  currency: string; // Currency ID
+  type: string;
+  category: string;
+  agent?: string; // Agent's ID
+  rooms?: number; // Real estate
+  bathrooms?: number; // Real estate
+  livingRooms?: number; // Real estate
+  description: string;
+  images: string[]; // Array of image IDs,
+  owner?: string; // Owner's ID. Farm produce
+  status: string;
+  created: Date;
+  updated: Date;
+}
