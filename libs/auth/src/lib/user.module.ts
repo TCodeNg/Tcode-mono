f2bbef251dbb2620c5326a1419dc67761edae5a2
Thompson Edolo <verygreenboi@live.com>
2020-06-13 23:48:54 +0100

    fix(global): disable hashing

--- libs/auth/src/lib/user.module.ts
+++ libs/auth/src/lib/user.module.ts
@@ -17,19 +17,19 @@ import * as bcrypt from 'bcrypt';
         name: 'User',
         useFactory: () => {
           const schema = UserSchema;
-          schema.pre<User>('save', async function(next: Function) {
-            if (this.isModified('password')) {
-              try {
-                this.password = await bcrypt.hash(this.password, 10);
-                this.createdAt = new Date();
-                this.updatedAt = new Date();
-                next();
-              } catch (e) {
-                console.log(e);
-                next(e);
-              }
-            }
-          });
+          // schema.pre<User>('save', async function(next: Function) {
+          //   if (this.isModified('password')) {
+          //     try {
+          //       this.password = await bcrypt.hash(this.password, 10);
+          //       this.createdAt = new Date();
+          //       this.updatedAt = new Date();
+          //       next();
+          //     } catch (e) {
+          //       console.log(e);
+          //       next(e);
+          //     }
+          //   }
+          // });
           return schema;
         }
       }
