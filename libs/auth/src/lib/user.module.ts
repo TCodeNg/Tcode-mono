3e78cd16d0ac374886a99162420bab7877ec1f68
Thompson Edolo <verygreenboi@live.com>
2020-06-13 23:08:12 +0100

    fix(global): pass error to callback

--- libs/auth/src/lib/user.module.ts
+++ libs/auth/src/lib/user.module.ts
@@ -19,10 +19,14 @@ import * as bcrypt from 'bcrypt';
           const schema = UserSchema;
           schema.pre<User>('save', async function(next: Function) {
             if (this.isModified('password')) {
-              this.password = await bcrypt.hash(this.password, 10);
+              try {
+                this.password = await bcrypt.hash(this.password, 10);
+                next();
+              } catch (e) {
+                console.log(e);
+                next(e);
+              }
             }
-            console.log(this);
-            next();
           });
           return schema;
         }
