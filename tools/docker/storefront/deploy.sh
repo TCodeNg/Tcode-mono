9d8cfcd606419713d668468fd0959dce7b9f6674
Thompson Edolo <verygreenboi@live.com>
2020-07-06 00:11:20 +0100

    fix(storefront): fix deployment script

--- tools/docker/storefront/deploy.sh
+++ tools/docker/storefront/deploy.sh
@@ -3,7 +3,7 @@ set -e
 echo $PWD
 rm -rf ./dist/apps/tmp
 mkdir ./dist/apps/tmp
-cp ./dist/apps/storefront/* ./dist/apps/tmp
+cp -r ./dist/apps/storefront/* ./dist/apps/tmp
 rm -rf ./dist/apps/storefront/*
 cp tools/docker/storefront/nginx.conf.template dist/apps/storefront/nginx.conf.template
 cp tools/docker/storefront/Dockerfile dist/apps/storefront/Dockerfile
