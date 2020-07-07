122c8c8a563e1eaf1b36262aa626e402b02ce3dd
Thompson Edolo <verygreenboi@live.com>
2020-07-07 18:28:06 +0100

    chore(root): clean up

--- apps/storefront/src/app/app.module.ts
+++ apps/storefront/src/app/app.module.ts
@@ -4,18 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './Shared/shared.module';
-import { MatIconModule } from '@angular/material/icon';
-import {MatBadgeModule} from '@angular/material/badge';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    SharedModule,
-    MatIconModule,
-    MatBadgeModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
   ],
   providers: [],
