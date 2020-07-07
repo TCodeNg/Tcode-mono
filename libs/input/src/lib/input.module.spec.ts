bb05f27a35178dd2a571c796b1d557ab763713e2
Sulaiman <sulaimain@kobo360.com>
2020-07-07 16:24:53 +0100

    ui refactor

--- libs/input/src/lib/input.module.spec.ts
+++ libs/input/src/lib/input.module.spec.ts
@@ -2,6 +2,7 @@ import { async, TestBed } from '@angular/core/testing';
 import { InputModule } from './input.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 describe('InputModule', () => {
   beforeEach(async(() => {
@@ -9,7 +10,8 @@ describe('InputModule', () => {
       imports: [
         InputModule, 
         MatIconModule, 
-        MatMenuModule
+        MatMenuModule,
+        MatTooltipModule
       ],
     }).compileComponents();
   }));
--- libs/input/src/lib/input.module.ts
+++ libs/input/src/lib/input.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatMenuModule} from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import { InputComponent } from './input/input.component';
 import { ErrorRefDirective } from './inputError.directive';
 import { PhoneNumberComponent } from './phone-number/phone-number.component';
 import { CountryFlagPathPipe } from './country-flag-path.pipe';
 
 @NgModule({
-  imports: [CommonModule, MatMenuModule, MatIconModule],
+  imports: [CommonModule, MatMenuModule, MatIconModule, MatTooltipModule ],
   declarations: [
     InputComponent, ErrorRefDirective, PhoneNumberComponent, CountryFlagPathPipe
   ],
