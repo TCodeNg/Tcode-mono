c0c72f957da98b475a12e02fc716c6929885f4af
Sulaiman <sulaimain@kobo360.com>
2020-07-26 08:39:28 +0100

    footer component

--- apps/storefront/src/app/becomeAVendor/becomeaVendor.module.ts
+++ apps/storefront/src/app/becomeAVendor/becomeaVendor.module.ts
@@ -16,7 +16,6 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    BrowserAnimationsModule,
     BecomeAVendorRoutingModule,
     InputModule,
     MatRadioModule,
