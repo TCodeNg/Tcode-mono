0ed573cd8df94a46bbfbdbc67d2b1219bdfe81a2
Sulaiman <sulaimain@kobo360.com>
2020-11-11 22:39:48 +0100

    Complete payment flow WIP

--- apps/storefront/src/app/payment/payment.component.ts
+++ apps/storefront/src/app/payment/payment.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
+import { ActivatedRoute } from '@angular/router';
+import { OrderService, ORDER_SERVICE_TOKEN } from '@tcode/order';
 
 @Component({
      selector: 'tcode-payment',
@@ -6,9 +8,28 @@ import { Component, OnInit } from "@angular/core";
      styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
-     constructor(){}
+     orderId: string;
+     order;
+     constructor(
+          private activeRoute: ActivatedRoute,
+          @Inject(ORDER_SERVICE_TOKEN) private orderService: OrderService
+     ){
+          this.orderId = activeRoute.snapshot.queryParams.orderId;
+     }
 
      ngOnInit(): void {
-          
+          this.getOrder(this.orderId);
+     }
+
+     async getOrder(orderId: string): Promise<any> {
+          try {
+               this.order = await this.orderService.getOrder(this.orderId).toPromise();
+          } catch (error) {
+               console.log(error);
+          }
+     }
+
+     async pay(){
+          console.log(this.order);
      }
 }
