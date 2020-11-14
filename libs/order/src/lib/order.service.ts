bd52510c5c3d85045aa761bcb6f5a6c4d2b4083b
Thompson Edolo <verygreenboi@live.com>
2020-11-14 11:21:36 +0100

    fix: return typed order

--- libs/order/src/lib/order.service.ts
+++ libs/order/src/lib/order.service.ts
@@ -7,7 +7,7 @@ export interface OrderServiceInterface {
   getOrders(skip?: number, limit?: number, filters?: Filter): Observable<Order[]>;
   getOrder(orderId: string): Observable<any>;
   createOrder(payload: any): Observable<string>;
-  watchOrder(orderId: string): Observable<any>;
+  watchOrder(orderId: string): Observable<Order>;
 }
 
 export type OrderService = OrderServiceInterface;
