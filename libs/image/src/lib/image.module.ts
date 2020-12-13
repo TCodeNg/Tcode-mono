dba8ca7442c6ae554238e16c82f7002f3e86e959
Thompson Edolo <verygreenboi@live.com>
2020-12-14 00:02:01 +0100

    fix: remove multi flag from service provider

--- libs/image/src/lib/image.module.ts
+++ libs/image/src/lib/image.module.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageService } from './image.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ImageServiceConfig } from './image.service.config';
 import { IMAGE_CONFIG_TOKEN } from './image-config.token';
 import { ImageInterceptor } from './image.interceptor';
@@ -15,11 +15,14 @@ export class ImageModule {
     return {
       ngModule: ImageModule,
       providers: [
-        ImageInterceptor,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: ImageInterceptor,
+          multi: true
+        },
         {
           provide: MediaService,
-          useClass: ImageService,
-          multi: true
+          useClass: ImageService
         },
         {
           provide: IMAGE_CONFIG_TOKEN,
